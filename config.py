702770a48586c1a43693544c43318579a14c3b97
ankush1284 <ankushsharma968234@gmail.com>
2025-04-25 01:56:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
+import os
+
 class Config:
     API_ID = int(os.environ.get("API_ID", ""))
     API_HASH = os.environ.get("API_HASH", "")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    BOT_OWNER = int(os.environ.get("BOT_OWNER", ""))
+    BOT_OWNER = int(os.environ.get("BOT_OWNER", "0"))  # Default to "0" to avoid ValueError
     DATABASE_URI = os.environ.get("DATABASE_URI", "")
     DATABASE_NAME = os.environ.get("DATABASE_NAME", "vj-forward-bot")
