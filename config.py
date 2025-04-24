8efacd6d13505def6c00f621adc4c8a74973af2d
ankush1284 <ankushsharma968234@gmail.com>
2025-04-25 02:58:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,3 +7,6 @@ class Config:
     BOT_OWNER = int(os.environ.get("BOT_OWNER", "0"))  # Default to "0" to avoid ValueError
     DATABASE_URI = os.environ.get("DATABASE_URI", "")
     DATABASE_NAME = os.environ.get("DATABASE_NAME", "vj-forward-bot")
+
+# This fixes the ImportError in your code
+temp = {}
